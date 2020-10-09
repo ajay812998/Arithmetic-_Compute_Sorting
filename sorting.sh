942d121712dbe0da113d0bdcc129639a54491af4
ajay812998 <ajay812998@gmail.com>
2020-10-09 21:04:18 +0530

    Added : Store results in dictionary

--- sorting.sh
+++ sorting.sh
@@ -6,10 +6,10 @@ read -p "Enter first input: " a
 read -p "Enter second input: " b
 read -p "Enter third input: " c
 
-echo "$(((a+b)*c))"
+declare -A dictionary
+dictionary[uc2]=$(((a+b)*c))
+dictionary[uc3]=$(((a*b)+c))
+dictionary[uc4]=$(((c+a)/b))
+dictionary[uc5]=$(((a/b)+c))
 
-echo "$(((a*b)+c))"
-
-echo "$(((c+a)/b))"
-
-echo "$(((a/b)+c))"
+echo "${dictionary[@]}"
