b59c1cf8dc13837c94c90fba7c5842aea6cd1338
ajay812998 <ajay812998@gmail.com>
2020-10-09 21:16:32 +0530

    Added : Values from Dictionary into array

--- sorting.sh
+++ sorting.sh
@@ -13,3 +13,12 @@ dictionary[uc4]=$(((c+a)/b))
 dictionary[uc5]=$(((a/b)+c))
 
 echo "${dictionary[@]}"
+
+count=0
+array[count++]=${dictionary[uc2]}
+array[count++]=${dictionary[uc3]}
+array[count++]=${dictionary[uc4]}
+array[count++]=${dictionary[uc5]}
+
+echo "Array" ${array[@]}
+echo "Count" $count
