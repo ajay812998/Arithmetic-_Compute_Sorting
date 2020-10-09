6a433b2efcd534b4bae97d5d8976ae97e39cbd8a
ajay812998 <ajay812998@gmail.com>
2020-10-09 20:20:53 +0530

    Added : Three Inputs

--- sorting.sh
+++ sorting.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -x
 
 #This problem computes different arithmetic expressions and sorts the results
+
+read -p "Enter first input: " a
+read -p "Enter second input: " b
+read -p "Enter third input: " c
+
