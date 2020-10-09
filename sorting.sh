c6d831579549494a4f9d0b97a844b6a887ff2700
ajay812998 <ajay812998@gmail.com>
2020-10-09 21:22:59 +0530

    Added : Result in Descending Order

--- sorting.sh
+++ sorting.sh
@@ -22,3 +22,18 @@ array[count++]=${dictionary[uc5]}
 
 echo "Array" ${array[@]}
 echo "Count" $count
+
+for (( i=0; i<count-1; i++ ));
+do
+
+  for (( j=0; j<count-i-1; j++ ));
+do
+     if (( array[j] < array[j+1] )); then
+        temp=${array[j]}
+        array[j]=${array[j+1]}
+        array[j+1]=$temp
+     fi
+  done
+done
+
+echo "Computation Result in Descending Order is" ${array[@]}
